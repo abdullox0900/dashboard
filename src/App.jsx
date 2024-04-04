e95bca184e76358e705eebc9d1774b7fe52604c1
abdullox0900 <abdullox0900@gmail.com>
2024-04-04 23:20:52 +0500

    fix bag

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
 					<Route path='abdullokh' element={<Abdullokh />}></Route>
 					<Route path='data' element={<Data />} />
 					<Route path='dev' element={<Dev />} />
+
 					<Route path='english' element={<English />} />
 					<Route path='website' element={<Website />} />
 				</Route>
